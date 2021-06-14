23638af7bd2f8a4d624a8ecb9464abca9c9dce5a
Fredrik Oseberg <fredrik.no@gmail.com>
2021-06-14 15:53:08 +0200

    feat: setup base app for SPA application

--- src/api/unleash.js
+++ src/api/unleash.js
@@ -1,4 +1,4 @@
-import api, {storage} from '@forge/api';
+import api, { storage } from '@forge/api';
 
 const NOT_FOUND = 404;
 
@@ -7,125 +7,138 @@ const API_KEY_STORAGE_KEY = 'unleash_api_key';
 const CUSTOM_FIELD_STORAGE_KEY = 'unleash_custom_field';
 
 const getApi = async () => {
-    return await storage.get(API_URL_STORAGE_KEY);
-}
+  return await storage.get(API_URL_STORAGE_KEY);
+};
 
 const getApiUrl = async (subpath) => `${await getApi()}${subpath}`;
 const getToggleUrl = async (toggleName) => {
-    return `${await getApiUrl('/api/admin/features')}/${toggleName}`;
-}
+  return `${await getApiUrl('/api/admin/features')}/${toggleName}`;
+};
 
 const getFrontendFeatureUrl = async (toggleName) => {
-    return await getApiUrl(`/features/strategies/${toggleName}`);
-}
+  return await getApiUrl(`/features/strategies/${toggleName}`);
+};
 
 const getCreateUrl = async () => await getApiUrl('/api/admin/features');
 
-const getArchiveUrl = async (issueKey) => `${await getApiUrl('/api/admin/archive/features')}/${issueKey}`;
+const getArchiveUrl = async (issueKey) =>
+  `${await getApiUrl('/api/admin/archive/features')}/${issueKey}`;
 
 const getProjectUrl = async () => await getApiUrl('/api/admin/projects');
 
-const bootstrapUrl = async () => await getApiUrl('/api/admin/ui-bootstrap');
+const bootstrapUrl = async () =>
+  await getApiUrl('/api/admin/ui-bootstrap');
 
 const getApiKey = async () => {
-    return await storage.get(API_KEY_STORAGE_KEY);
-}
+  return await storage.get(API_KEY_STORAGE_KEY);
+};
 
 const getAuth = async () => {
-    const apiKey = await getApiKey();
-    return { headers: { 'Authorization': apiKey }};
-}
+  const apiKey = await getApiKey();
+  return { headers: { Authorization: apiKey } };
+};
 
 const getCustomField = async () => {
-    return await storage.get(CUSTOM_FIELD_STORAGE_KEY);
-}
+  return await storage.get(CUSTOM_FIELD_STORAGE_KEY);
+};
 
 const saveCustomField = async (fieldId) => {
-    await storage.set(CUSTOM_FIELD_STORAGE_KEY, fieldId);
-}
+  await storage.set(CUSTOM_FIELD_STORAGE_KEY, fieldId);
+};
 
 const getArchivedToggle = async (issueKey) => {
-    const archived = await api.fetch(await getArchiveUrl(issueKey), await getAuth());
-    return archived.ok;
-}
+  const archived = await api.fetch(
+    await getArchiveUrl(issueKey),
+    await getAuth()
+  );
+  return archived.ok;
+};
 
 const fetchFeatureToggle = async (issueKey) => {
-    const res = await api.fetch(await getToggleUrl(issueKey), await getAuth());
-    if (res.ok) {
-        console.info('Successfully fetched toggle');
-        const data = await res.json();
-        return { enabled: data.enabled, errors: false, found: true, creatable: false };
-    } else if (res.status === NOT_FOUND) {
-        console.info('404ed, check archived');
-        const archived = await getArchivedToggle(issueKey);
-        return { enabled: false, found: false, errors: false, creatable: !archived.ok };
-    } else {
-        console.info(`Errored ${res.status}`);
-        return { enabled: false, found: false, errors: true, creatable: false };
-    }
-}
+  const res = await api.fetch(
+    await getToggleUrl(issueKey),
+    await getAuth()
+  );
+  if (res.ok) {
+    console.info('Successfully fetched toggle');
+    const data = await res.json();
+    return { enabled: data.enabled, errors: false, found: true };
+  } else if (res.status === NOT_FOUND) {
+    console.info('404ed, check archived');
+    const archived = await getArchivedToggle(issueKey);
+    return {
+      enabled: false,
+      found: false,
+      errors: false,
+      archived: archived.ok,
+    };
+  } else {
+    console.info(`Errored ${res.status}`);
+    return { enabled: false, found: false, errors: true };
+  }
+};
 
 const createFeatureToggle = async (toggleData) => {
-    const unleashApiKey = await getApiKey();
-    const featureRequest = {
-        name: toggleData.name,
-        enabled: toggleData.enabled || false,
-        project: toggleData.project || 'default',
-        description: toggleData.description,
-        strategies: [{ name: 'default' }]
-    };
-    console.info(`Creating feature toggle ${toggleData.name}`);
-    const data = await api.fetch(await getCreateUrl(), {
-        headers: { 'Authorization': unleashApiKey },
-        body: JSON.stringify(featureRequest),
-        method: 'POST'
-    });
-    return data.ok;
-}
+  const unleashApiKey = await getApiKey();
+  const featureRequest = {
+    name: toggleData.name,
+    enabled: toggleData.enabled || false,
+    project: toggleData.project || 'default',
+    description: toggleData.description,
+    strategies: [{ name: 'default' }],
+  };
+  console.info(`Creating feature toggle ${toggleData.name}`);
+  const data = await api.fetch(await getCreateUrl(), {
+    headers: { Authorization: unleashApiKey },
+    body: JSON.stringify(featureRequest),
+    method: 'POST',
+  });
+  return data.ok;
+};
 
 const fetchProjects = async () => {
-    const unleashApiKey = await getApiKey();
-    const data= await api.fetch(await getProjectUrl(), {
-        headers: { 'Authorization': unleashApiKey },
+  const unleashApiKey = await getApiKey();
+  const data = await api.fetch(await getProjectUrl(), {
+    headers: { Authorization: unleashApiKey },
+  });
+  if (data.ok) {
+    const json = await data.json();
+    return json.projects.map((p) => {
+      return {
+        id: p.id,
+        name: p.name,
+      };
     });
-    if (data.ok) {
-        const json = await data.json()
-        return json.projects.map(p => {
-            return {
-                id: p.id,
-                name: p.name
-            };
-        });
-    }
-    return []
-}
+  }
+  return [];
+};
 
 const fetchUiBootstrap = async () => {
-    const unleashApiKey = await getApiKey();
-    const data = await api.fetch(await bootstrapUrl(), {
-        headers: { 'Authorization': unleashApiKey },
-    });
-    if (data.ok) {
-        const json = await data.json();
-        return {
-            featureTypes: json.featureTypes,
-            projects: json.projects,
-        }
-    }
-}
+  const unleashApiKey = await getApiKey();
+  const data = await api.fetch(await bootstrapUrl(), {
+    headers: { Authorization: unleashApiKey },
+  });
+  if (data.ok) {
+    const json = await data.json();
+    return {
+      featureTypes: json.featureTypes,
+      projects: json.projects,
+    };
+  }
+};
 
 export const unleash = {
-    fetchFeatureToggle,
-    createFeatureToggle,
-    fetchProjects,
-    fetchUiBootstrap,
-    getApi,
-    getApiUrl,
-    getApiKey,
-    getCustomField,
-    saveCustomField,
-    getFrontendFeatureUrl,
-    API_KEY_STORAGE_KEY,
-    API_URL_STORAGE_KEY,
-    CUSTOM_FIELD_STORAGE_KEY,
+  fetchFeatureToggle,
+  createFeatureToggle,
+  fetchProjects,
+  fetchUiBootstrap,
+  getApi,
+  getApiUrl,
+  getApiKey,
+  getCustomField,
+  saveCustomField,
+  getFrontendFeatureUrl,
+  API_KEY_STORAGE_KEY,
+  API_URL_STORAGE_KEY,
+  CUSTOM_FIELD_STORAGE_KEY,
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,124 +1,238 @@
 import ForgeUI, {
-    Badge,
-    Form,
-    Fragment,
-    IssuePanel,
-    Link,
-    Option,
-    Select,
-    Text,
-    TextArea,
-    TextField,
-    Toggle,
-    render,
-    useProductContext,
-    useState
+  Badge,
+  Form,
+  Fragment,
+  IssuePanel,
+  Link,
+  Option,
+  Select,
+  Button,
+  Text,
+  TextArea,
+  TextField,
+  Toggle,
+  render,
+  useProductContext,
+  useState,
+  ModalDialog,
 } from '@forge/ui';
 import { storage } from '@forge/api';
-import { unleash } from "./api/unleash";
+import { unleash } from './api/unleash';
 
-
-const UnleashCommunicationFailure = () => <Text>Could not reach Unleash API</Text>;
-
+const UnleashCommunicationFailure = () => (
+  <Text>Could not reach Unleash API</Text>
+);
 
 const UnleashToggleStatus = ({ issueKey, toggleName, enabled }) => {
-    const [featureUrl, setFeatureUrl] = useState(async () => {
-        return await unleash.getFrontendFeatureUrl(toggleName);
-    })
-    return (
-        <Fragment>
-            <Text>
-                <Link href={featureUrl}>{toggleName}</Link> is {enabled ? <Badge text='enabled' appearance='added'/> : <Badge text ='disabled' appearance='removed' /> }
-            </Text>
-        </Fragment>
-    );
+  const [featureUrl, setFeatureUrl] = useState(async () => {
+    return await unleash.getFrontendFeatureUrl(toggleName);
+  });
+  return (
+    <Fragment>
+      <Text>
+        <Link href={featureUrl}>{toggleName}</Link> is{' '}
+        {enabled ? (
+          <Badge text='enabled' appearance='added' />
+        ) : (
+          <Badge text='disabled' appearance='removed' />
+        )}
+      </Text>
+    </Fragment>
+  );
 };
 
 const CreateToggle = ({ uiConfig, issueKey, setFeature }) => {
-    return (<Fragment>
-        <Text>The feature toggle for {issueKey} does not exist.</Text>
-        <Form onSubmit={async (formData) => {
-            await storage.set(`unleash_toggle_${issueKey}`, formData.name);
-            await unleash.createFeatureToggle(formData);
-            setFeature({ creatable: false, enabled: formData.enabled, found: true, name: formData.name, errors: false });
-        }} submitButtonText="Create feature toggle">
-            <Select label="Project" name="project" isRequired={true}>
-                {uiConfig.projects.map(({ id, name }) =>
-                    <Option label={name} value={id} defaultSelected={id === 'default'}/>
-                )}
-            </Select>
-            <Select label="Toggle type" name="type" isRequired={true}>
-                {uiConfig.featureTypes.map(({ id, name }) =>
-                    <Option label={name} value={id} defaultSelected={id === 'release'} />
-                )}
-            </Select>
-            <Toggle label="Enable feature" name="enabled" defaultChecked={false}/>
-            <TextField label="Toggle name" name="name" isRequired={true}/>
-            <TextArea label="Toggle description" name="description"/>
-        </Form>
-    </Fragment>);
-}
-
-const CannotCreateToggle = ({ issueKey, toggleName }) => <Text>Can not create toggle for {issueKey}. A toggle already exists with
-    the name {toggleName}</Text>;
-
-const CreatableToggle = ({ uiConfig, creatable, issueKey, setFeature, toggleName }) => {
-    return (<Fragment>{creatable
-        ? <CreateToggle uiConfig={uiConfig} issueKey={issueKey} toggleName={toggleName} setFeature={setFeature}/>
-        : <CannotCreateToggle toggleName={toggleName} issueKey={issueKey}/>}
-    </Fragment>);
+  return (
+    <Fragment>
+      <Text>The feature toggle for {issueKey} does not exist.</Text>
+      <Form
+        onSubmit={async (formData) => {
+          await storage.set(`unleash_toggle_${issueKey}`, formData.name);
+          await unleash.createFeatureToggle(formData);
+          setFeature({
+            creatable: false,
+            enabled: formData.enabled,
+            found: true,
+            name: formData.name,
+            errors: false,
+          });
+        }}
+        submitButtonText='Create feature toggle'
+      >
+        <Select label='Project' name='project' isRequired={true}>
+          {uiConfig.projects.map(({ id, name }) => (
+            <Option
+              label={name}
+              value={id}
+              defaultSelected={id === 'default'}
+            />
+          ))}
+        </Select>
+        <Select label='Toggle type' name='type' isRequired={true}>
+          {uiConfig.featureTypes.map(({ id, name }) => (
+            <Option
+              label={name}
+              value={id}
+              defaultSelected={id === 'release'}
+            />
+          ))}
+        </Select>
+        <Toggle
+          label='Enable feature'
+          name='enabled'
+          defaultChecked={false}
+        />
+        <TextField label='Toggle name' name='name' isRequired={true} />
+        <TextArea label='Toggle description' name='description' />
+      </Form>
+    </Fragment>
+  );
 };
 
-const UnleashToggle = ({ uiConfig, setFeature, found, issueKey, enabled, creatable, toggleName }) => {
-    return (<Fragment>
-        {found
-            ? <UnleashToggleStatus toggleName={toggleName} issueKey={issueKey} enabled={enabled}/>
-            : <CreatableToggle toggleName={toggleName} uiConfig={uiConfig} creatable={creatable} issueKey={issueKey} setFeature={setFeature}/>
-        }
-    </Fragment>);
+const FeatureToggle = ({
+  uiConfig,
+  archived,
+  issueKey,
+  setFeature,
+  toggleName,
+}) => {
+  const [createModal, setCreateModal] = useState(false);
+  const [existingToggle, setExistingToggle] = useState(false);
+
+  return (
+    <Fragment>
+      <Button
+        text='Create and connect feature toggle'
+        onClick={() => setCreateModal(true)}
+      />
+
+      <Button
+        text='Connect existing toggle'
+        onClick={() => setExistingToggle(true)}
+      />
+
+      {createModal ? (
+        <ModalDialog
+          header='Create toggle'
+          onClose={() => setCreateModal(false)}
+          onSubmit
+        >
+          <CreateToggle
+            uiConfig={uiConfig}
+            issueKey={issueKey}
+            toggleName={toggleName}
+            setFeature={setFeature}
+          />
+        </ModalDialog>
+      ) : null}
+      {existingToggle ? (
+        <ModalDialog
+          header='Create toggle'
+          onClose={() => setExistingToggle(false)}
+          onSubmit
+        >
+          <LinkExistingToggle
+            uiConfig={uiConfig}
+            issueKey={issueKey}
+            toggleName={toggleName}
+            setFeature={setFeature}
+          />
+        </ModalDialog>
+      ) : null}
+    </Fragment>
+  );
+};
+
+const LinkExistingToggle = () => {
+  return (
+    <Fragment>
+      <div>Hello world</div>
+    </Fragment>
+  );
+};
+
+const UnleashToggle = ({
+  uiConfig,
+  setFeature,
+  found,
+  issueKey,
+  enabled,
+  toggleName,
+}) => {
+  return (
+    <Fragment>
+      {found ? (
+        <UnleashToggleStatus
+          toggleName={toggleName}
+          issueKey={issueKey}
+          enabled={enabled}
+        />
+      ) : (
+        <FeatureToggle
+          toggleName={toggleName}
+          uiConfig={uiConfig}
+          issueKey={issueKey}
+          setFeature={setFeature}
+        />
+      )}
+    </Fragment>
+  );
 };
 
 const FeatureToggleComponent = ({ issueKey, toggleName }) => {
-    const [uiConfig, setUiConfig] = useState(async () => {
-        return await unleash.fetchUiBootstrap();
-    })
-    const [feature, setFeature] = useState(async () => {
-        return await unleash.fetchFeatureToggle(toggleName);
-    });
-    return (
-        <Fragment>
-            {feature.errors ? <UnleashCommunicationFailure/> :
-                <UnleashToggle toggleName={feature.name || toggleName}
-                               uiConfig={uiConfig}
-                               setFeature={setFeature}
-                               found={feature.found}
-                               creatable={feature.creatable}
-                               issueKey={issueKey}
-                               enabled={feature.enabled}
-                />}
-        </Fragment>
-    )
-}
+  const [uiConfig, setUiConfig] = useState(async () => {
+    return await unleash.fetchUiBootstrap();
+  });
+  const [feature, setFeature] = useState(async () => {
+    return await unleash.fetchFeatureToggle(toggleName);
+  });
+  return (
+    <Fragment>
+      {feature.errors ? (
+        <UnleashCommunicationFailure />
+      ) : (
+        <UnleashToggle
+          toggleName={feature.name || toggleName}
+          uiConfig={uiConfig}
+          setFeature={setFeature}
+          found={feature.found}
+          archived={feature.archived}
+          issueKey={issueKey}
+          enabled={feature.enabled}
+        />
+      )}
+    </Fragment>
+  );
+};
 
 const App = () => {
-    const context = useProductContext();
-    const issueKey = context.platformContext.issueKey;
-    const [toggleName, setToggleName] = useState(async () => {
-        return await storage.get(`unleash_toggle_${issueKey}`);
-    })
-    const [apiUrl, _setApiUrl] = useState(async () => {
-        return await unleash.getApi();
-    })
+  const context = useProductContext();
+  const issueKey = context.platformContext.issueKey;
+  const [toggleName, setToggleName] = useState(async () => {
+    return await storage.get(`unleash_toggle_${issueKey}`);
+  });
+  const [apiUrl, _setApiUrl] = useState(async () => {
+    return await unleash.getApi();
+  });
 
-    return <Fragment>
-            {apiUrl ?
-            <FeatureToggleComponent toggleName={toggleName} issueKey={issueKey}/>
-                : <Text>You need to configure Unleash using the app's admin page</Text>}
-        </Fragment>;
+  return (
+    <Fragment>
+      {apiUrl ? (
+        <FeatureToggleComponent
+          toggleName={toggleName}
+          issueKey={issueKey}
+        />
+      ) : (
+        <Text>
+          You need to configure Unleash using the app's admin page
+        </Text>
+      )}
+    </Fragment>
+  );
 };
 
 export const run = render(
-    <IssuePanel>
-        <App/>
-    </IssuePanel>
+  <IssuePanel>
+    <App />
+  </IssuePanel>
 );
